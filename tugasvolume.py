1ee4391ee75c9be4bd42cdcaa4a090daf0e39d41
feradefi <113221593+feradefi@users.noreply.github.com>
2022-09-15 09:48:50 +0700

    Add files via upload

---
+++ tugasvolume.py
@@ -0,0 +1,22 @@
+#Fera Defi
+#SI kloting 3
+
+#Kubus
+input("Menghitung Volume Kubus")
+s = int(input("masukan panjang sisi kubus = "))
+
+#Volume Kubus
+V = s * s * s
+print("volume kubus: " + str (V))
+
+
+#Balok
+input("Menghitung Volume Balok")
+p = int(input("masukan panjang balok = "))
+l = int(input("masukan lebar balok = "))
+t = int(input("masukan tinggi balok = "))
+
+#Volume Balok
+V = p * l * t
+print("volume balok: " + str (V))
+
